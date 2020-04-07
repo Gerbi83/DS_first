f81d4ebb10fdde40a9f07c3b9cb5e074a4f4143a
gerbi83 <gerbior@gmail.com>
2020-04-07 12:59:12 +0300

    pdf added

---
+++ DataStructors/DataStructors/listNode.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#include "listNode.h"
+
+ListNode::ListNode()
+{
+	value = -1;
+	next = previous = nullptr;
+}
+
+ListNode::ListNode(int vl)
+{
+	value = vl;
+	next = previous = nullptr;
+}
+
+ListNode* ListNode::getNext()
+{
+	return next;
+}
+
+ListNode* ListNode::getPrevious()
+{
+	return previous;
+}
+
+void ListNode::setNext(ListNode& a)
+{
+	next = &a;
+}
+
+int ListNode::getValue()
+{
+	return value;
+}
